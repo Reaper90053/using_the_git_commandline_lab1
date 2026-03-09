d3ed3759d73c1841bb91d703e5ac28df64ca758e
Стас <stasgarbuz18012000@gmail.com>
2026-03-09 23:35:03 +0200

    Updated Book class

--- LibraryLab2/Book.cpp
+++ LibraryLab2/Book.cpp
@@ -1,16 +1,56 @@
 #include "Book.h"
-#include <iostream>
 
-Book::Book() : Book("Невідома", "Невідомий", 0) {} // делегування
+int Book::bookCount = 0;
 
-Book::Book(string t, string a, int y)
-    : title(t), author(a), year(y) {} // список ініціалізації
+Book::Book() : title("Unknown"), author("Unknown"), year(0) {
+    bookCount++;
+}
 
-Book::~Book() {
-    cout << "Об'єкт Book знищено\n";
+Book::Book(string title, string author, int year)
+    : title(title), author(author), year(year) {
+    bookCount++;
+}
+
+Book::Book(const Book& other)
+    : title(other.title), author(other.author), year(other.year) {
+    bookCount++;
+}
+
+Book::Book(Book&& other)
+    : title(move(other.title)), author(move(other.author)), year(other.year) {
+    other.year = 0;
+}
+
+Book::~Book() {}
+
+void Book::setTitle(string title) {
+    this->title = title;
 }
 
 void Book::printInfo() const {
-    cout << "Назва: " << title << "\nАвтор: " << author
-         << "\nРік: " << year << "\n\n";
+    cout << "Title: " << title << endl;
+    cout << "Author: " << author << endl;
+    cout << "Year: " << year << endl;
+}
+
+int Book::getBookCount() {
+    return bookCount;
+}
+
+Book Book::operator+(const Book& other) {
+    return Book(title + " & " + other.title, author, year);
+}
+
+Book Book::operator-() {
+    return Book("Removed: " + title, author, year);
+}
+
+ostream& operator<<(ostream& os, const Book& book) {
+    os << book.title << " " << book.author << " " << book.year;
+    return os;
+}
+
+istream& operator>>(istream& is, Book& book) {
+    is >> book.title >> book.author >> book.year;
+    return is;
 }
--- LibraryLab2/Book.h
+++ LibraryLab2/Book.h
@@ -2,20 +2,42 @@
 #define BOOK_H
 
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class Book {
+
 private:
     string title;
     string author;
     int year;
 
+    static int bookCount;
+
 public:
-    Book(); // конструктор
-    Book(string t, string a, int y = 0); // перевантажений з параметром за замовчуванням
-    ~Book(); // деструктор
+
+    Book();
+    Book(string title, string author, int year);
+
+    Book(const Book& other); // Copy constructor
+    Book(Book&& other);      // Move constructor
+
+    ~Book();
 
     void printInfo() const;
+
+    void setTitle(string title);
+
+    static int getBookCount();
+
+    // оператори
+    Book operator+(const Book& other);
+    Book operator-();
+
+    // дружні оператори
+    friend ostream& operator<<(ostream& os, const Book& book);
+    friend istream& operator>>(istream& is, Book& book);
 };
 
 #endif
