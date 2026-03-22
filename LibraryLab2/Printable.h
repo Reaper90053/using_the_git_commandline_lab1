8a2105d854ec7d43d46b77078d20e25e3b9dd732
Стас <stasgarbuz18012000@gmail.com>
2026-03-22 23:21:55 +0200

    Added Printable interface with pure virtual functions

---
+++ LibraryLab2/Printable.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "Printable.h"
+using namespace std;
+
+class Printable {
+public:
+    virtual void printData() const = 0;
+};
+
+#ifndef LIBRARYLAB2_PRINTABLE_H
+#define LIBRARYLAB2_PRINTABLE_H
+
+#endif //LIBRARYLAB2_PRINTABLE_H
