647cbbb7dba4b3e9972e3c289de9b6df91e219e0
Стас <stasgarbuz18012000@gmail.com>
2026-04-02 00:27:26 +0300

    Refactor Library to use smart pointers (shared_ptr)

--- LibraryLab2/Library.cpp
+++ LibraryLab2/Library.cpp
@@ -1,21 +1,45 @@
 #include "Library.h"
 #include <iostream>
+#include <fstream>
 
 Library::Library() : Library("Untitled", "Unknown address") {}
 
 Library::Library(string name, string address)
     : name(name), address(address) {}
 
-void Library::addBook(const Book& book) {
-    books.push_back(book);
+void Library::addBook(shared_ptr<Book> book) {
+    books.push_back(book);//6 лаба
 }
 
 void Library::showBooks() const {
     for (const auto& b : books) {
-        b.printInfo();
+        b->printInfo();//6 лаба стрілка
     }
 }
+void Library::saveToFile() {
+    ofstream file("books.txt");
 
+    for (const auto& b : books) {
+        file << b->getTitle() << " "
+             << b->getAuthor() << " "
+             << b->getYear() << endl;
+    }
+
+    file.close();
+}// 6 лаба
+
+void Library::loadFromFile() {
+    ifstream file("books.txt");
+
+    string title, author;
+    int year;
+
+    while (file >> title >> author >> year) {
+        books.push_back(make_shared<Book>(title, author, year));
+    }
+
+    file.close();
+}// 6 лаба
 
 Library::~Library() {
     cout << "Object Library destroyed\n";
--- LibraryLab2/Library.h
+++ LibraryLab2/Library.h
@@ -3,6 +3,7 @@
 
 #pragma once
 #include <iostream>
+#include <memory>
 #include <vector>
 #include "Book.h"
 using namespace std;
@@ -11,19 +12,22 @@ class Library {
 private:
     string name;
     string address;
-    vector<Book> books;
+    vector<shared_ptr<Book>> books;//6 лаба це і є смарт поінтери
 
 public:
     Library();
     Library(string name, string addr = "Unknown address");
     ~Library();
 
-    void addBook(const Book& book);
+    void addBook(shared_ptr<Book> book);//6 лаба
     void showBooks() const;
 
     static int getLibraryCount();
 
     void printInfo() const;
+
+    void saveToFile();
+    void loadFromFile();//6 лаба
 };
 
 #endif
